6c172b8bb124d45976350e778a2ee508b6e9438d
Jang Tae Won <xodnjs1837@gmail.com>
2023-10-24 10:43:13 +0900

    feat : 스프링 초기 설정

---
+++ BackEnd/kkori/src/test/java/com/kkori/kkori/KkoriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkori.kkori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkoriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
