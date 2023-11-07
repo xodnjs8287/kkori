687455d35ec481b06885703b8b5244e06045d68f
Jang Tae Won <xodnjs1837@gmail.com>
2023-11-07 10:58:30 +0900

    feat : S09P31C110-157/be/feat/jobpost 등록

--- BackEnd/src/main/java/com/kkori/kkori/jobboard/controller/JobBoardController.java
+++ BackEnd/src/main/java/com/kkori/kkori/jobboard/controller/JobBoardController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -19,13 +20,15 @@ public class JobBoardController {
 
     private final JobBoardService jobBoardService;
 
-    @PostMapping("/post-job")
+    @PostMapping(value = "/post-job", consumes = {"multipart/form-data", "application/json"})
     public ResponseEntity<RegisterJobBoardResponse> register(
             final Authentication authentication,
-            @RequestBody RegisterJobBoardRequest request) {
+            @ModelAttribute RegisterJobBoardRequest request,
+            @RequestParam(required = false) MultipartFile file) {
         Long name = Long.parseLong(authentication.getName());
         return ResponseEntity.ok(jobBoardService.register(name, request));
     }
+
 
     @GetMapping("/post/all")
     public ResponseEntity<List<RegisterJobBoardResponse>> findAll(){
